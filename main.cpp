2d8e1e09b6e47da1c49c98ca901901406fadc5b6
Provodock <159698285+Provodock@users.noreply.github.com>
2024-10-13 00:00:40 +0300

    add calculator lab2

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "calculator.h"
+
+int main() {
+    Calculator calc;
+    double a = 10.5;
+    double b = 5.2;
+    
+    std::cout << "Addition: " << calc.Add(a, b) << std::endl;
+    std::cout << "Subtraction: " << calc.Sub(a, b) << std::endl;
+    
+    return 0;
+}
